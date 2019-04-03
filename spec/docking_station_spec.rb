9758918ca779bad4651a597cda01bc4c0fcb09a4
yadlra <yadi.sb1@gmail.com>
2019-04-03 11:34:35 +0100

    spec changes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,6 @@
 require 'docking_station'
-
 describe DockingStation do
-  it 'responds to release bike' do
-    expect(subject).to respond_to :release_bike
-  end
+ it 'responds to release bike' do
+   expect(subject).to respond_to :release_bike
+ end
 end
