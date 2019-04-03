5fe5ef8686bc742bb024d0dfdeb657a2e0f96cdc
yadlra <yadi.sb1@gmail.com>
2019-04-03 15:36:11 +0100

    docking stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,13 @@
 require 'docking_station'
-describe DockingStation do
- it 'responds to release bike' do
-   expect(subject).to respond_to :release_bike
- end
+require 'bike'
+
+RSpec.describe DockingStation do
+  describe '#release_bike' do
+    it 'responds to release bike' do
+      expect(subject).to respond_to :release_bike
+    end
+     it 'release a bike' do
+       expect(subject.release_bike).to be_a(Bike)
+     end
+   end
 end
